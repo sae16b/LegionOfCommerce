e139e92ec1b7f74ffd0b5b092944e1420df8749d
sae16b <44991814+sae16b@users.noreply.github.com>
2019-07-20 21:17:53 -0400

    test commit

---
+++ LegionOfCommerce/Extensions/ServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LegionOfCommerce.Extensions
+{
+    public static class ServiceExtensions
+    {
+		public static void ConfigureSqlContext(IServiceCollection services, IConfiguration config)
+		{
+
+		}
+    }
+}
